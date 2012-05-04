34d1fde347d1f4c651844f59251a96b957ec7138
marimo <nitta@dd.mbn.or.jp>
2012-05-04 10:43:33 +0900

    bool型の読み込みに対応

--- Excel/lib/WorkSheet.rb
+++ Excel/lib/WorkSheet.rb
@@ -10,7 +10,12 @@ class Cell
 		@xml = xml
 	end
 	def value
-		return @xml.elements[1].text.to_i
+		case @xml.attributes['t']
+		when nil
+			@xml.elements[1].text.to_i
+		when 'b'
+			@xml.elements[1].text != '0'
+		end
 	end
 end
 
@@ -30,10 +35,16 @@ class WorkSheet
 	
 	def cell_value(ref)
 		cell = Cell.new(@xml.elements.to_a('//c').find{|c| c.attributes['r'] == ref.to_s})
+		p cell
 		return cell.value
 	end
 	
-		def method_missing(method_name)
-		self.cell_value(method_name) || super
+	def method_missing(method_name)
+		value = self.cell_value(method_name)
+		if value.nil? then
+			super
+		else
+			value
+		end
 	end
 end
--- Excel/spec/WorkSheet_spec.rb
+++ Excel/spec/WorkSheet_spec.rb
@@ -9,54 +9,63 @@ describe WorkSheet do
 		book.close
 	end
 	
-	subject { book.Sheet1}
+	let(:sheet1) { book.Sheet1}
 	let(:book) { WorkBook.open(test_file('Book1')) }
 	let(:sheet2) { book.Sheet2 }
-	let(:other) { book.sheets[2] } 
+	let(:data) { book.sheets[2] } 
 	
 	describe '#name' do
 		it 'でシート名が取得できる。' do
-			subject.name.should == 'Sheet1'
+			sheet1.name.should == 'Sheet1'
 			sheet2.name.should == 'Sheet2'
 		end
 		
-		it 'で日本語で指定したシート名が取得できる。' do
-			other.name.should == 'いろいろなデータ'.encode("Shift_JIS")
+		it 'で日本語で指定したシート名がShift_JISで取得できる。' do
+			data.name.should == 'いろいろなデータ'.encode("Shift_JIS")
 		end
 	end
 	
 	describe '#cell_value' do
 		it 'はセルの値を取得する。' do
-			subject.cell_value('C3').should == 4
+			sheet1.cell_value('C3').should == 4
 		end
 		
 		it 'は列方向に正しいセルを取得する。' do
-			subject.cell_value('A1').should == 1
-			subject.cell_value('C1').should == 2
+			sheet1.cell_value('A1').should == 1
+			sheet1.cell_value('C1').should == 2
 		end
 		
 		it 'は行方向に正しいセルを取得する。' do
-			subject.cell_value('A1').should == 1
-			subject.cell_value('A3').should == 3
+			sheet1.cell_value('A1').should == 1
+			sheet1.cell_value('A3').should == 3
 		end
 		
 		it 'はシートを区別して正しいセルを取得する。' do
-			subject.cell_value('A1').should == 1
+			sheet1.cell_value('A1').should == 1
 			sheet2.cell_value('A1').should == 5
 		end
 		
 		it 'はシンボルを渡しても動作する。' do
-			subject.cell_value(:C3).should == 4
+			sheet1.cell_value(:C3).should == 4
 		end
 	end
 	
 	describe '#セル名' do
-		it 'はセルの値を取得する。' do
-			subject.A1.should == 1
-			subject.C3.should == 4
-		end
 		it 'は存在しないセル名を指定した時にNoMethodErrorを返す' do
-			lambda { subject.a1 }.should raise_error NoMethodError
+			lambda { sheet1.a1 }.should raise_error NoMethodError
+		end
+		
+		context 'が値を取得する時' do
+			it 'に数値を取得できる。' do
+				sheet1.A1.should == 1
+				sheet1.C3.should == 4
+			end
+			it 'にtrueを取得できる。' do
+				data.A1.should == true
+			end
+			it 'にfalseを取得できる。' do
+				data.B1.should == false
+			end
 		end
 	end
 end
