d9ee2e913ec6e3b9d2acd26305dbae71531e95cb
nitta mitsuhiro <nitta@dd.mbn.or.jp>
2012-05-30 21:32:40 +0900

    スペックコードのUTF-8対応の削除
    いつの間にかなくても正常に表示されるようになっていた

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,13 +7,3 @@ require 'work_book'
 def test_file(file_name)
   "./spec/test_data/#{file_name}.xlsx"
 end
-
-def message_in_sjis
-  class << self
-    alias __it__ it
-    
-    def it(message)
-      __it__(message.to_s.encode('Shift_JIS')){ yield if block_given? }
-    end
-  end
-end
