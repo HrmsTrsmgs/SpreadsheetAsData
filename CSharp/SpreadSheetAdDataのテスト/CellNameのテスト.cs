fdca217fdab0688ef61750b344cc776645e0be15
NITTA Mitsuhiro <potimarimo@gmail.com>
2015-02-26 13:30:58 +0900

    CellNameクラスの追加

---
+++ CSharp/SpreadSheetAdDataのテスト/CellNameのテスト.cs
@@ -0,0 +1,36 @@
+﻿using Marimo.SpreadSheetAsData;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace Marimo.SpreadSheetAdData.Test
+{
+    [TestFixture]
+    public class CellNameのテスト : AssertionHelper
+    {
+        [Test]
+        public void ColumnNameプロパティで列名を取得できます()
+        {
+            Expect(new CellName("A1").ColumnName, Is.EqualTo("A"));
+            Expect(new CellName("B1").ColumnName, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void ColumnIndexプロパティで列番号を取得できます()
+        {
+            Expect(new CellName("A1").ColumnIndex, Is.EqualTo(1));
+            Expect(new CellName("B1").ColumnIndex, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void RowIndexプロパティで列番号を取得できます()
+        {
+            Expect(new CellName("A1").RowIndex, Is.EqualTo(1));
+            Expect(new CellName("A2").RowIndex, Is.EqualTo(2));
+        }
+    }
+}
---
+++ CSharp/SpreadSheetAsData/CellName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Marimo.SpreadSheetAsData
+{
+    public class CellName
+    {
+        string name;
+
+        public CellName(string name)
+        {
+            this.name = name;
+        }
+
+        public uint ColumnIndex
+        {
+            get
+            {
+                return (uint)(ColumnName.First() - 'A') + 1;
+            }
+        }
+
+        public string ColumnName
+        {
+            get
+            {
+                return Regex.Match(name, @"[A-Z]").Value;
+            }
+        }
+
+        public uint RowIndex
+        {
+            get
+            {
+                return uint.Parse(Regex.Match(name, @"\d").Value);
+            }
+        }
+    }
+}
