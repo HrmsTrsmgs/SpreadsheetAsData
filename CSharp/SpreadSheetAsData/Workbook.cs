7f902bce3b4b0f77a04021e95373d3bea722e369
新田光宏 <potimarimo@gmail.com>
2020-11-08 13:32:08 +0900

    Workbookのリファクタリング

--- CSharp/SpreadSheetAsData/Workbook.cs
+++ CSharp/SpreadSheetAsData/Workbook.cs
@@ -1,51 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using Packaging =  DocumentFormat.OpenXml.Packaging;
+using Packaging = DocumentFormat.OpenXml.Packaging;
 using Spreadsheet = DocumentFormat.OpenXml.Spreadsheet;
 
 namespace Marimo.SpreadSheetAsData
 {
     public class Workbook : IDisposable
     {
-        public static Workbook Open(string filePath)
-        {
-            return new Workbook { Document = Packaging.SpreadsheetDocument.Open(filePath, true) };
-        }
+        private bool disposedValue;
+        public static Workbook Open(string filePath) =>
+            new Workbook { Document = Packaging.SpreadsheetDocument.Open(filePath, true) };
 
         internal Packaging.SpreadsheetDocument Document { get; set; }
 
         WorksheetCollection sheets { get; set; }
 
-        public WorksheetCollection Sheets
-        {
-            get
-            {
-                if(sheets == null)
-                {
-                    sheets = new WorksheetCollection(
+        public WorksheetCollection Sheets =>
+            sheets ??= new WorksheetCollection(
                         from sheet in Document.WorkbookPart.Workbook.Sheets.Elements<Spreadsheet.Sheet>()
                         select new Worksheet { Book = this, Name = sheet.Name.Value });
-                }
-                return sheets;
-            }
-        }
-
+        
         public Worksheet this[int index] => Sheets[index];
 
         public Worksheet this[string sheetName] => Sheets[sheetName];
 
-        public void Close()
+        public void Close() => Document.Close();
+
+        protected virtual void Dispose(bool disposing)
         {
-            Document.Close();
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Close();
+                }
+                disposedValue = true;
+            }
         }
 
         public void Dispose()
         {
-            Close();
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
