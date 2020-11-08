2eb15e66e11da088529bbcc5772ce151d9ade4e0
新田光宏 <potimarimo@gmail.com>
2020-11-08 13:05:02 +0900

    Cellのリファクタリング

--- CSharp/SpreadSheetAsData/Cell.cs
+++ CSharp/SpreadSheetAsData/Cell.cs
@@ -1,13 +1,6 @@
 ﻿using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Spreadsheet;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-using Packaging = DocumentFormat.OpenXml.Packaging;
 using Spreadsheet = DocumentFormat.OpenXml.Spreadsheet;
 
 namespace Marimo.SpreadSheetAsData
@@ -20,73 +13,37 @@ namespace Marimo.SpreadSheetAsData
             Xml = xml;
         }
 
-        public Cell(Worksheet sheet, string cellReference)
-        {
-            Sheet = sheet;
-            Xml = new Spreadsheet.Cell(
-                new Spreadsheet.Value { })
-            {
-                CellReference = new StringValue(cellReference)
-            };
-        }
+        public Cell(Worksheet sheet, string cellReference) :
+            this(
+                sheet,
+                new Spreadsheet.Cell(new Value { })
+                {
+                    CellReference = new StringValue(cellReference)
+                })
+        { }
 
         internal Spreadsheet.Cell Xml { get;　private set; }
 
-        internal Spreadsheet.Row RowXml
-        {
-            get
-            {
-                return Xml.Parent as Spreadsheet.Row;
-            }
-        }
+        internal Spreadsheet.Row RowXml => Xml.Parent as Row;
 
-        public string Reference
-        {
-            get
-            {
-                return Xml.CellReference;
-            }
-        }
-        public dynamic Value
-        {
-            get
-            {
-                switch (Xml.DataType?.Value)
-                {
-                    case CellValues.Boolean:
-                        return Xml.CellValue.Text != "0";
-                    case CellValues.SharedString:
-                        return Book.Document.WorkbookPart.SharedStringTablePart.SharedStringTable.Elements<Spreadsheet.SharedStringItem>().ElementAt(int.Parse(Xml.CellValue.Text)).Text.Text;
-                    default:
-                        return double.Parse(Xml.CellValue.Text);
-                }   
-            }
-        }
+        public string Reference => Xml.CellReference;
 
+        public dynamic Value =>
+            Xml.DataType?.Value switch
+            {
+                CellValues.Boolean =>
+                    Xml.CellValue.Text != "0",
+                CellValues.SharedString =>
+                    Book.Document.WorkbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(int.Parse(Xml.CellValue.Text)).Text.Text,
+                _ => double.Parse(Xml.CellValue.Text)
+            };
+        
         public Worksheet Sheet { get; private set; }
 
-        public Workbook Book
-        {
-            get
-            {
-                return Sheet.Book;
-            }
-        }
+        public Workbook Book => Sheet.Book;
 
-        public uint RowIndex
-        {
-            get
-            {
-                return RowXml.RowIndex;
-            }
-        }
+        public uint RowIndex => RowXml.RowIndex;
 
-        public uint ColumnIndex
-        {
-            get
-            {
-                return CellName.Parse(Reference).ColumnIndex;
-            }
-        }
+        public uint ColumnIndex => CellName.Parse(Reference).ColumnIndex;
     }
 }
