e8922e3b7372985e064a9d8206041a7fd279e7d0
新田光宏 <potimarimo@gmail.com>
2020-11-08 19:17:24 +0900

    BlankValueと少数の0との比較

--- CSharp/SpreadSheetAdDataのテスト/BlankValueのテスト.cs
+++ CSharp/SpreadSheetAdDataのテスト/BlankValueのテスト.cs
@@ -42,6 +42,12 @@ namespace SpreadSheetAdDataのテスト
         }
 
         [Test]
+        public void 比較すると少数の0と同じとされます()
+        {
+            Assert.That(tested == .0, Is.True);
+        }
+
+        [Test]
         public void 比較すると空白以外の文字列とは同じとされません()
         {
             Assert.That(tested == "a", Is.False);
--- CSharp/SpreadSheetAsData/BlankValue.cs
+++ CSharp/SpreadSheetAsData/BlankValue.cs
@@ -4,21 +4,25 @@ namespace Marimo.SpreadSheetAsData
 {
     public struct BlankValue
     {
-        public override bool Equals(object obj)
-        {
-            return obj is int && (int)obj == 0 || obj is string && (string)obj == "";
-        }
+        public override bool Equals(object obj) =>
+            obj switch
+            {
+                int i => this == i,
+                double d => this == d,
+                string s => this == s,
+                _ => false
+            };
+        
 
-        public static bool operator ==(BlankValue a, int b) => a.Equals(b);
+        public static bool operator ==(BlankValue a, int b) => b == 0;
 
         public static bool operator !=(BlankValue a, int b) => !(a == b);
 
-        public static bool operator ==(BlankValue a, string b) => a.Equals(b);
+        public static bool operator ==(BlankValue a, string b) => b == "";
 
         public static bool operator !=(BlankValue a, string b) => !(a == b);
 
-
-        public static bool operator ==(BlankValue a, double b) => a.Equals(b);
+        public static bool operator ==(BlankValue a, double b) => b == 0;
 
         public static bool operator !=(BlankValue a, double b) => !(a == b);
     }
