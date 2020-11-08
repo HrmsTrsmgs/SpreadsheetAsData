ecc27f2c7db0bdd9a142be8065a83f39134acdae
新田光宏 <potimarimo@gmail.com>
2020-11-08 13:35:14 +0900

    Worksheetのリファクタリング

--- CSharp/SpreadSheetAsData/Worksheet.cs
+++ CSharp/SpreadSheetAsData/Worksheet.cs
@@ -13,29 +13,17 @@ namespace Marimo.SpreadSheetAsData
         public Worksheet()
         {
             Cells = new CellCollection(this);
-            Range = new CellRangeCollection();
         }
-
 
         public Workbook Book { get; internal set; }
         public string Name { get; internal set; }
-        public CellCollection Cells { get;private set; }
-        public CellRangeCollection Range { get; private set; }
+        public CellCollection Cells { get; }
+        public CellRangeCollection Range { get; } = new CellRangeCollection();
 
-        internal Spreadsheet.Sheet SheetTag
-        {
-            get
-            {
-                return Book.Document.WorkbookPart.Workbook.Descendants<Spreadsheet.Sheet>().Where(_ => _.Name == Name).Single();
-            }
-        }
+        internal Spreadsheet.Sheet SheetTag =>
+            Book.Document.WorkbookPart.Workbook.Descendants<Spreadsheet.Sheet>().Where(_ => _.Name == Name).Single();
 
-        internal Packaging.WorksheetPart WorksheetPart
-        {
-            get
-            {
-                return Book.Document.WorkbookPart.GetPartById(SheetTag.Id) as Packaging.WorksheetPart;
-            }
-        }
+        internal Packaging.WorksheetPart WorksheetPart =>
+            Book.Document.WorkbookPart.GetPartById(SheetTag.Id) as Packaging.WorksheetPart;
     }
 }
