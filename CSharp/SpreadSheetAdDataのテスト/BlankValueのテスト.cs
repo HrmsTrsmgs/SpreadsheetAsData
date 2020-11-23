be31cb017bf75f710e130c83a47f0c9a2de72285
新田光宏 <potimarimo@gmail.com>
2020-11-23 18:25:02 +0900

    floatの0と比較

--- CSharp/SpreadSheetAdDataのテスト/BlankValueのテスト.cs
+++ CSharp/SpreadSheetAdDataのテスト/BlankValueのテスト.cs
@@ -54,6 +54,12 @@ namespace SpreadSheetAdDataのテスト
         }
 
         [Test]
+        public void 比較するとfloatの0と同じとされます()
+        {
+            Assert.That(tested == 0f, Is.True);
+        }
+
+        [Test]
         public void 比較すると空白以外の文字列とは同じとされません()
         {
             Assert.That(tested == "a", Is.False);
