98fe15233b6937aca4b0177f44d670ce4c7badb2
新田光宏 <potimarimo@gmail.com>
2020-11-28 20:09:02 +0900

    リファクタリング

--- CSharp/SpreadSheetAsData/BlankValue.cs
+++ CSharp/SpreadSheetAsData/BlankValue.cs
@@ -3,15 +3,7 @@
 namespace Marimo.SpreadSheetAsData
 {
     public struct BlankValue
-    {
-        public override bool Equals(object obj) =>
-            obj switch
-            {
-                int i => this == i,
-                double d => this == d,
-                string s => this == s,
-                _ => false
-            };
+    { 
         public static implicit operator string(BlankValue _) => "";
 
         public static implicit operator double(BlankValue _) => .0;
