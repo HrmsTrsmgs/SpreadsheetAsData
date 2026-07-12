1bb69db56d26e15b3f2d5eb62fea46182b8a82ae
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2026-07-13 05:26:52 +0900

    test: 最低限の実装でなかったワークシートスコープCell対応を保留に戻す

--- CSharp/SpreadSheetAsData/CellCollection.cs
+++ CSharp/SpreadSheetAsData/CellCollection.cs
@@ -76,7 +76,7 @@ public class CellCollection
                 }
                 catch (FormatException)
                 {
-                    return sheet.ResolveNamedRange(cellReference).SingleCell;
+                    return sheet.ResolveNamedRange(cellReference).TopLeftCell;
                 }
             }
 
