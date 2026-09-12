cba928a1c09bf4f910ba85d78254d0f70274bfa4
NITTA Mitsuhiro <hrmstrsmgs@gmail.com>
2026-09-12 19:52:38 +0900

    空白を含む小数列をnullableなdoubleプロパティとして生成する

--- CSharp/SpreadSheetAsData.CodeGeneration/WorkbookWrapperComponents.cs
+++ CSharp/SpreadSheetAsData.CodeGeneration/WorkbookWrapperComponents.cs
@@ -375,6 +375,11 @@ static class WorkbookWrapperComponents
             return "double";
         }
 
+        if (values.All(it => it is double or BlankValue))
+        {
+            return "double?";
+        }
+
         if (values.All(it => it is bool))
         {
             return "bool";
--- CSharp/SpreadSheetAsData.CodeGenerationのテスト/コード生成型推論のテスト.cs
+++ CSharp/SpreadSheetAsData.CodeGenerationのテスト/コード生成型推論のテスト.cs
@@ -191,6 +191,19 @@ public sealed class コード生成型推論のテスト
     }
 
     [Fact]
+    public void 小数値と空白を持つ列をnullableなdoubleプロパティとして生成します()
+    {
+        var tested = GeneratedCodeInspection
+            .AssemblyFrom(
+                GeneratedCodeInspection.GenerateSources(@"TestData\テーブル.xlsx"))
+            .GeneratedType("空白数値マッピング")
+            .GetProperty("小数");
+
+        tested.Should().NotBeNull();
+        tested.PropertyType.Should().Be(typeof(double?));
+    }
+
+    [Fact]
     public void 列名と生成プロパティ名が一致する場合は列属性を生成しません()
     {
         var tested = GeneratedCodeInspection
